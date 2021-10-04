87dec4a7bf7d8f5218b4fe73fee140b62237441a
Tomáš Svoboda <323969@muni.cz>
2021-10-04 16:02:30 +0200

    Request methods encapsulated to special module, debug refactoring

---
+++ request.py
@@ -0,0 +1,50 @@
+from pprint import pprint
+import requests
+import setting
+
+def process_url(url, headers):
+    if "oneprovider/" in url:
+        url = setting.ONEPROVIDER_API_URL + url
+        headers.update(setting.ONEPROVIDER_AUTH_HEADERS)
+    elif "onepanel/" in url:
+        url = setting.ONEPANEL_API_URL + url
+        headers.update(setting.ONEPANEL_AUTH_HEADERS)
+    elif "onezone/" in url:
+        url = setting.ONEZONE_API_URL + url
+        headers.update(setting.ONEZONE_AUTH_HEADERS)
+    else:
+        print("Error: no comunication party in url")
+        return None
+    return url, headers
+
+def response_print(response):
+    if setting.DEBUG >= 1:
+        if not response.ok:
+            print("Warning: response is not ok, response code", response.status_code)
+
+def debug_print(response):
+    if setting.DEBUG >= 3:
+        print(response)
+        if response.content != b'':
+            pprint(response.json())
+
+def get(url, headers=dict()):
+    url, headers = process_url(url, headers)
+    response = requests.get(url, headers=headers, verify=False)
+    response_print(response)
+    debug_print(response)
+    return response
+
+def patch(url, headers=dict(), data=dict()):
+    url, headers = process_url(url, headers)    
+    response = requests.patch(url, headers=headers, data=data, verify=False)
+    response_print(response)
+    debug_print(response)
+    return response
+
+def post(url, headers=dict(), data=dict()):
+    url, headers = process_url(url, headers)    
+    response = requests.post(url, headers=headers, data=data, verify=False)
+    response_print(response)
+    debug_print(response)
+    return response
--- spaces.py
+++ spaces.py
@@ -1,28 +1,30 @@
 import json
 import sys
 import time
+import requests
 from setting import *
+import request
 import tokens
 
 def getSpaces():
-    if DEBUG: print("getSpaces(): ")
+    if DEBUG >= 2: print("getSpaces(): ")
     # https://onedata.org/#/home/api/stable/oneprovider?anchor=operation/get_all_spaces
-    url = ONEPROVIDER_API_URL + "oneprovider/spaces"
-    response = requests.get(url, headers=ONEZONE_AUTH_HEADERS, verify=False)
-    if DEBUG: print(response)
-    if DEBUG: pprint(response.json())
+    url = "oneprovider/spaces"
+    response = request.get(url)
     return response.json()
 
 def removeSpace(space_id):
-    if DEBUG: print("removeSpace(" + space_id + "): ")
+    if DEBUG >= 2: print("removeSpace(" + space_id + "): ")
     # https://onedata.org/#/home/api/stable/onezone?anchor=operation/remove_space
     url = ONEZONE_API_URL + "onezone/spaces/" + space_id
     response = requests.delete(url, headers=ONEZONE_AUTH_HEADERS, verify=False)
-    if DEBUG: print(response)
+    if DEBUG >= 3: 
+        print(response)
+        pprint(response, response.json())
     return response
 
 def getSpace(space_id):
-    if DEBUG: print("getSpace(" + space_id + "): ")
+    if DEBUG >= 2: print("getSpace(" + space_id + "): ")
     # https://onedata.org/#/home/api/stable/oneprovider?anchor=operation/get_space
     url = ONEPROVIDER_API_URL + "oneprovider/spaces/" + space_id
     timeout = 3
@@ -31,46 +33,56 @@ def getSpace(space_id):
         if response.ok:
             timeout = 0
         else:
-            if DEBUG: print("Timeouted", str(4-timeout), "ouf of 3 times;" )
+            print("Error: timeouted", str(4-timeout), "ouf of 3 times;" )
             timeout = timeout - 1
-            time.sleep(5)
-    if DEBUG: pprint(response.json())
+            time.sleep(10)
+    if DEBUG >= 3: 
+        print(response)
+        pprint(response, response.json())
     return response.json()
 
 def getSpaceDetails(space_id):    
-    if DEBUG: print("getSpaceDetails(" + space_id + "): ")
+    if DEBUG >= 2: print("getSpaceDetails(" + space_id + "): ")
     # https://onedata.org/#/home/api/stable/onepanel?anchor=operation/get_space_details
     url = ONEPROVIDER_API_URL + "onepanel/provider/spaces/" + space_id
     response = requests.get(url, headers=ONEPANEL_AUTH_HEADERS, verify=False)
-    if DEBUG: pprint(response.json())
+    if DEBUG >= 3: 
+        print(response)
+        pprint(response, response.json())
     return response.json()
 
 def getAutoStorageImportInfo(space_id):
-    if DEBUG: print("getAutoStorageImportInfo(" + space_id + "): ")
+    if DEBUG >= 2: print("getAutoStorageImportInfo(" + space_id + "): ")
     # https://onedata.org/#/home/api/stable/onepanel?anchor=operation/get_auto_storage_import_info
     url = ONEPROVIDER_API_URL + "onepanel/provider/spaces/" + space_id + "/storage-import/auto/info"
     response = requests.get(url, headers=ONEPANEL_AUTH_HEADERS, verify=False)
-    if DEBUG: pprint(response.json())
+    if DEBUG >= 3: 
+        print(response)
+        pprint(response, response.json())
     return response.json()
 
 def downloadFileContent(file_id):
-    if DEBUG: print("downloadFileContent(" + file_id + "): ")
+    if DEBUG >= 2: print("downloadFileContent(" + file_id + "): ")
     # https://onedata.org/#/home/api/stable/oneprovider?anchor=operation/download_file_content
     url = ONEPROVIDER_API_URL + "oneprovider/data/" + file_id + "/content"
     response = requests.get(url, headers=ONEZONE_AUTH_HEADERS, verify=False)
-    if DEBUG: pprint(response)
+    if DEBUG >= 3: 
+        print(response)
+        pprint(response, response.json())
     return response
 
 def listDirectory(file_id):
-    if DEBUG: print("listDirectory(" + file_id + "): ")
+    if DEBUG >= 2: print("listDirectory(" + file_id + "): ")
     # https://onedata.org/#/home/api/stable/oneprovider?anchor=operation/list_children
     url = ONEPROVIDER_API_URL + "oneprovider/data/" + file_id + "/children"
     response = requests.get(url, headers=ONEZONE_AUTH_HEADERS, verify=False)
-    if DEBUG: pprint(response.json())
+    if DEBUG >= 3: 
+        print(response)
+        pprint(response, response.json())
     return response.json()
 
 def createSpaceForGroup(group_id, space_name):
-    if DEBUG: print("createSpaceForGroup(" + group_id + ", " + space_name + "): ")
+    if DEBUG >= 2: print("createSpaceForGroup(" + group_id + ", " + space_name + "): ")
     # https://onedata.org/#/home/api/stable/onezone?anchor=operation/create_space_for_group
     url = ONEZONE_API_URL + "onezone/groups/" + group_id + "/spaces"
     my_data = {'name': space_name}
@@ -80,13 +92,13 @@ def createSpaceForGroup(group_id, space_name):
     # Should return space ID in Headers
     location = resp.headers["Location"]
     space_id = location.split("spaces/")[1]
-    if DEBUG: print(space_id)
+    if DEBUG >= 1: print("Created space", space_name, "with id", space_id)
     return space_id
 
 def supportSpace(token, size, storage_id):
-    if DEBUG: print("supportSpace(" + token + ", " + str(size) + ", " + storage_id + "): ")
+    if DEBUG >= 2: print("supportSpace(token, " + str(size) + ", " + storage_id + "): ")
     # https://onedata.org/#/home/api/stable/onepanel?anchor=operation/support_space
-    url = ONEPANEL_API_URL + "onepanel/provider/spaces"
+    url = "onepanel/provider/spaces"
     data = {
         'token': token["token"],
         'size': size,
@@ -98,21 +110,20 @@ def supportSpace(token, size, storage_id):
             }
         }
     }
-    my_headers = dict(ONEPANEL_AUTH_HEADERS)
-    my_headers['Content-type'] = 'application/json'
-    resp = requests.post(url, headers=my_headers, data=json.dumps(data), verify=False)
+    
+    headers = dict({'Content-type': 'application/json'})
+    resp = request.post(url, headers=headers, data=json.dumps(data))
     #return resp.json()["id"]
 
 def setNewSizeToSpace(space_id, size):
-    if DEBUG: print("setNewSpaceSize(" + space_id + ", " + str(size) + "): ")
+    if DEBUG >= 2: print("setNewSpaceSize(" + space_id + ", " + str(size) + "): ")
     # based on https://onedata.org/#/home/api/stable/onepanel?anchor=operation/modify_space
-    url = ONEPANEL_API_URL + "onepanel/provider/spaces/" + space_id
+    url = "onepanel/provider/spaces/" + space_id
     data = {
         'size': size
     }
-    my_headers = dict(ONEPANEL_AUTH_HEADERS)
-    my_headers['Content-type'] = 'application/json'
-    resp = requests.patch(url, headers=my_headers, data=json.dumps(data), verify=False)
+    headers = dict({'Content-type': 'application/json'})
+    resp = request.patch(url, headers=headers, data=json.dumps(data))
     return resp
 
 def createAndSupportSpaceForGroup(name, group_id, storage_id, capacity):
@@ -129,7 +140,7 @@ def setSizeOfSpaceByDataSize(space_id):
     if sd['importedStorage'] == True and getAutoStorageImportInfo(space_id)['status'] == "completed":
         new_size = sd['spaceOccupancy']
         setNewSizeToSpace(space_id, new_size)
-        if DEBUG: print("Set up new size", new_size, "for space", space_id)
+        if DEBUG >= 1: print("Set up new size", new_size, "for space", space_id)
 
 def enableContinuousImport(space_id):
     if not getContinuousImportStatus(space_id):
@@ -146,19 +157,18 @@ def getContinuousImportStatus(space_id):
         if DEBUG: pprint(space_import_info)
         return space_import_info['storageImport']['autoStorageImportConfig']['continuousScan']
     else:
-        print("Space is not imported.")
+        if DEBUG >= 1: print("Warning: Value not available, space is not imported.")
         return None
 
 def setContinuousImport(space_id, newValue):
-    if DEBUG: print("setContinuousImport(" + space_id + ", " + str(newValue) + "): ")
+    if DEBUG >= 2: print("setContinuousImport(" + space_id + ", " + str(newValue) + "): ")
     # https://onedata.org/#/home/api/21.02.0-alpha21/onepanel?anchor=operation/modify_space
-    url = ONEPANEL_API_URL + "onepanel/provider/spaces/" + space_id
+    url = "onepanel/provider/spaces/" + space_id
     data = {
         'autoStorageImportConfig': {
             'continuousScan': newValue,
             'scanInterval': CONFIG['importingFiles']['scanInterval'],
-            }
+        }
     }
-    my_headers = dict(ONEPANEL_AUTH_HEADERS)
-    my_headers['Content-type'] = 'application/json'
-    resp = requests.patch(url, headers=my_headers, data=json.dumps(data), verify=False)
+    headers = dict({'Content-type': 'application/json'})
+    resp = request.patch(url, headers=headers, data=json.dumps(data))
