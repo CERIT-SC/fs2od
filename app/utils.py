59be919f35b011e59a4c41a7cf7a25d882827f97
Tomáš Svoboda <323969@muni.cz>
2021-11-13 13:47:47 +0100

    Preliminary logger

---
+++ app/utils.py
@@ -0,0 +1,19 @@
+from setting import Settings
+
+class Logger():
+
+    @staticmethod
+    def log(level, message):
+        prefix = ""
+        if level == 1: 
+            prefix = "INFO"
+        elif level == 2: 
+            prefix = "DEBUG"
+        elif level == 3: 
+            prefix = "ERROR"
+        else: 
+            prefix = ""
+        prefix = "["+prefix+"]"
+
+        if Settings.get().debug >= level:
+            print(prefix, message)
