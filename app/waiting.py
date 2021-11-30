c5867db21f00edb967df6438a6d1ae9b307b443f
Tomáš Svoboda <323969@muni.cz>
2021-11-30 07:53:51 +0100

    Quick fix

--- app/waiting.py
+++ app/waiting.py
@@ -13,9 +13,10 @@ run_time = datetime.datetime.now() - container_start
 # compare the uptime time of the container and given limit
 if run_time >= limit:
     # container is runnig longer time than given limit
-    if Settings.get().debug >= 2: print("waiting - time limit reached, exiting")
+    pass
+    #if Settings.get().debug >= 2: print("waiting - time limit reached, exiting")
 else:
     # container is running shorter time than given limit
     waiting_time = (limit - run_time).total_seconds()
-    if Settings.get().debug  >= 2: print("waiting - limit not reached, waiting", waiting_time)
+    #if Settings.get().debug  >= 2: print("waiting - limit not reached, waiting", waiting_time)
     time.sleep(waiting_time)
