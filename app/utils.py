6e60c13444bb979733672c226402aa275ff2c82b
Tomáš Svoboda <323969@muni.cz>
2022-05-12 11:36:15 +0200

    Add logging capability

--- app/utils.py
+++ app/utils.py
@@ -4,16 +4,19 @@ class Logger():
 
     @staticmethod
     def log(level, message):
-        prefix = ""
-        if level == 1: 
-            prefix = "INFO"
-        elif level == 2: 
-            prefix = "DEBUG"
-        elif level == 3: 
-            prefix = "ERROR"
-        else: 
-            prefix = ""
-        prefix = "["+prefix+"]"
-
+        """
+        Print the message if the global verbose level is equal or greater then the given level.
+        """
         if Settings.get().debug >= level:
+            prefix = ""
+            if level == 1: 
+                prefix = "Error"
+            elif level == 2: 
+                prefix = "Warning"
+            elif level == 3: 
+                prefix = "Info"
+            elif level >= 4:
+                prefix = "Debug"
+
+            prefix = "[" + prefix + "]"
             print(prefix, message)
