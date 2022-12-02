e90baf3699adab2ecbfcc569cd1a515f20468129
Tomáš Svoboda <323969@muni.cz>
2022-12-02 13:41:22 +0100

    Fix in sandbox

--- app/sandbox.py
+++ app/sandbox.py
@@ -7,12 +7,12 @@ from utils import Logger
 import storages, spaces, metadata, tokens, filesystem, groups, shares, files, transfers, request, workflow
 
 def sandbox(args):
-    """ 
+    """
     Sandbox method where specific workflows (experiments, tests, ...) can be done.
     """
     var1 = args.var1
     var2 = args.var2
-    var3 = args.var2
+    var3 = args.var3
     print("Run sandbox with variables:")
     print("var1=%s, var2=%s, var3=%s" % (var1, var2, var3))
     print("***")
