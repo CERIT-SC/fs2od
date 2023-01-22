15778f46f02c7713e488a7d400643b36e49841ef
Tomáš Svoboda <323969@muni.cz>
2023-01-22 19:45:33 +0100

    new devel path on ceitec1

--- push_sources.sh
+++ push_sources.sh
@@ -1,3 +1,3 @@
 # sync source files to server
 # call like ./push_sources.sh abc.node.eu
-rsync -azv --exclude={'__pycache__','.git','.vscode'} $PWD/ $USER@$1:~/fs2od/devel/sources
+rsync -azv --exclude={'__pycache__','.git','.vscode'} $PWD/ $USER@$1:~/onedata/fs2od/devel/sources
