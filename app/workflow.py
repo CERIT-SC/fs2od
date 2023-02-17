1d96d90e4164b73befb0620a7d84983ef99508c0
Jozef Sabo <536644@mail.muni.cz>
2023-02-17 17:54:39 +0100

    Added actions_log to workflow, BROKEN CODE

--- app/workflow.py
+++ app/workflow.py
@@ -5,7 +5,7 @@ from pprint import pprint
 from string import Template
 from settings import Settings
 from utils import Logger, Utils
-import spaces, storages, metadata, groups, tokens, shares, files, filesystem, dareg, qos
+import spaces, storages, metadata, groups, tokens, shares, files, filesystem, dareg, qos, actions_log
 
 
 def _get_storage_index(space_id: str, number_of_available_storages: int) -> int:
@@ -57,177 +57,222 @@ def _add_qos_requirement(space_id: str, replicas_number: int):
         )
 
 
-def registerSpace(base_path, directory):
-    full_path = base_path + os.sep + directory.name
-
+def registerSpace(base_path, directory) -> bool:
+    actions_logger = actions_log.get_actions_logger()
     # only directories should be processed
+    full_path = base_path + os.sep + directory.name
     if not os.path.isdir(full_path):
-        Logger.log(
-            3,
-            "Space can't be created, this isn't directory %s" % base_path + os.sep + directory.name,
-        )
-        return
+        Logger.log(3, f"Space can't be created, this isn't directory {full_path}")
+        return False
 
     # test if directory contains a yaml file
     yml_file = filesystem.getMetaDataFile(directory)
-    if yml_file:
-        yml_content = filesystem.loadYaml(yml_file)
-
-        # test if yaml contains space_id
-        if not filesystem.yamlContainsSpaceId(yml_content):
-            Logger.log(3, "Creating space from %s" % base_path + os.sep + directory.name)
-            dataset_name = Settings.get().config["datasetPrefix"] + directory.name
-
-            if not Utils.isValidOnedataName(dataset_name):
-                # try clear the name
-                dataset_name = Utils.clearOnedataName(dataset_name)
-                Logger.log(2, "Invalid dataset name %s. Renamed to %s" % (directory.name, dataset_name))
-                # and test it again
-                if not Utils.isValidOnedataName(dataset_name):
-                    Logger.log(2, "Dataset name %s can't be cleared" % directory.name)
-                    return
-
-            # Create a new space
-            space_id = spaces.createSpaceForGroup(
-                Settings.get().config["managerGroupId"], dataset_name
-            )
-            if Settings.get().config["dareg"]["enabled"] and space_id:
-                dareg.register_dataset(space_id, dataset_name, base_path)
-
-            time.sleep(1 * Settings.get().config["sleepFactor"])
-
-            # Create support token
-            support_token = tokens.createTemporarySupportToken(space_id)
-
-            # Create storage for the space
-            storage_id = storages.createAndGetStorage(
-                dataset_name, os.path.join(base_path, directory.name)
-            )
-            if Settings.get().config["dareg"]["enabled"] and storage_id:
-                dareg.log(space_id, "info", "created storage %s" % storage_id)
-            time.sleep(3 * Settings.get().config["sleepFactor"])
-
-            if space_id and support_token:
-                # set up space support on the provider
-                result_support = spaces.supportSpace(
-                    support_token, Settings.get().config["implicitSpaceSize"], storage_id, space_id
-                )
-                time.sleep(2 * Settings.get().config["sleepFactor"])
-
-                if Settings.get().config["dareg"]["enabled"] and result_support:
-                    dareg.log(space_id, "info", "supported")
-
-                if Settings.get().DATA_REPLICATION_ENABLED:
-                    Logger.log(
-                        3, "Setting up replication of space %s" % space_id
-                    )
-                    _add_support_from_all(support_token, space_id)
-                    _add_qos_requirement(space_id, Settings.get().DATA_REPLICATION_REPLICAS)
-
-                # HACK
-                if not result_support:
-                    # delete space
-                    spaces.removeSpace(space_id)
-                    time.sleep(2 * Settings.get().config["sleepFactor"])
-
-                    # delete storage
-                    storages.removeStorage(storage_id)
-                    Logger.log(
-                        1, "Space for %s not created (unsucessfull support)." % directory.name
-                    )
-                    if Settings.get().config["dareg"]["enabled"] and result_support:
-                        dareg.log(space_id, "error", "removed")
-                    return
-
-                # Create user group for space
-                gid = groups.createChildGroup(Settings.get().config["userGroupId"], dataset_name)
-                if Settings.get().config["dareg"]["enabled"] and gid:
-                    dareg.log(space_id, "info", "created group %s" % gid)
-                time.sleep(1 * Settings.get().config["sleepFactor"])
-
-                # Create invite token for the user group
-                token = tokens.createInviteTokenToGroup(gid, dataset_name)
-                if Settings.get().config["dareg"]["enabled"] and token:
-                    dareg.log(space_id, "info", "created invite token")
-                time.sleep(1 * Settings.get().config["sleepFactor"])
-
-                # add the space to the user group
-                privileges = [
-                    "space_view",
-                    "space_view_privileges",
-                    "space_read_data",
-                    "space_add_support",
-                ]
-                spaces.addGroupToSpace(space_id, gid, privileges)
-                # TODO - test result
-                if Settings.get().config["dareg"]["enabled"] and True:
-                    dareg.log(space_id, "info", "group added to space")
-                time.sleep(1 * Settings.get().config["sleepFactor"])
-
-                # write onedata parameters (space_id, invite_token) to file
-                yaml_onedata_dict = dict()
-                yaml_onedata_dict[Settings.get().config["metadataFileTags"]["space"]] = space_id
-                yaml_onedata_dict[Settings.get().config["metadataFileTags"]["inviteToken"]] = token[
-                    "token"
-                ]
-                filesystem.setValuesToYaml(yml_file, yml_content, yaml_onedata_dict)
-                time.sleep(3 * Settings.get().config["sleepFactor"])
-
-                # first import of files to Onedata space
-                spaces.startAutoStorageImport(space_id)
-                time.sleep(3 * Settings.get().config["sleepFactor"])
-
-                # set up permissions
-                file_id = spaces.getSpace(space_id)["fileId"]
-                files.setFileAttributeRecursive(
-                    file_id, Settings.get().config["initialPOSIXlikePermissions"]
-                )
-
-                # create public share
-                share = shares.createAndGetShare(dataset_name, file_id)
-                if not share:
-                    Logger.log(1, "Share for the space %s not created." % dataset_name)
-                    return
-
-                if Settings.get().config["dareg"]["enabled"]:
-                    dareg.update_dataset(space_id, token["token"], share["publicUrl"])
-
-                # add Share description
-                with open("share_description_base.md", "r") as file:
-                    to_substitute = {
-                        "dataset_name": dataset_name,
-                        "institution_name": Settings.get().config["institutionName"],
-                        "share_file_id": share["rootFileId"],
-                    }
-                    src = Template(file.read())
-                    result = src.substitute(to_substitute)
-                    shares.updateShare(share["shareId"], description=result)
-
-                # write onedata parameter (publicURL) to file
-                filesystem.setValueToYaml(
-                    yml_file,
-                    yml_content,
-                    Settings.get().config["metadataFileTags"]["publicURL"],
-                    share["publicUrl"],
-                )
-
-                # set metadata for the space
-                if Settings.get().config["importMetadata"]:
-                    metadata.setSpaceMetadataFromYaml(space_id)
-
-                path = base_path + os.sep + directory.name
-                Logger.log(3, "Processing of %s done." % path)
-                if Settings.get().config["dareg"]["enabled"]:
-                    dareg.log(space_id, "info", "processing done")
-                time.sleep(3 * Settings.get().config["sleepFactor"])
-            else:
-                Logger.log(1, "Space for %s not created." % directory.name)
-        else:
-            Logger.log(
-                4, "Space for %s not created (spaceId exists in yaml file)." % directory.name
-            )
-    else:
+    if not yml_file:
         Logger.log(4, "Space for %s not created (not contains yaml or no dir)." % directory.name)
+        return False
+
+    yml_content = filesystem.loadYaml(yml_file)
+
+    # test if yaml contains space_id
+    if filesystem.yamlContainsSpaceId(yml_content):
+        Logger.log(4, "Space for %s not created (spaceId exists in yaml file)." % directory.name)
+        return False
+
+    Logger.log(3, f"Creating space from {full_path}")
+    dataset_name = Settings.get().config["datasetPrefix"] + directory.name
+
+    if not Utils.isValidOnedataName(dataset_name):
+        # try clear the name
+        dataset_name = Utils.clearOnedataName(dataset_name)
+        Logger.log(2, f"Invalid dataset name {directory.name}. Changing to {dataset_name}")
+
+    # and test it again
+    if not Utils.isValidOnedataName(dataset_name):
+        Logger.log(2, f"Dataset name {directory.name} can't be cleared")
+        return False
+
+    space_name = Settings.get().TEST_PREFIX + dataset_name if Settings.get().TEST else dataset_name
+
+    actions_logger.log_pre("space", dataset_name)
+    # Create a new space
+    space_id = spaces.createSpaceForGroup(
+        group_id=Settings.get().config["managerGroupId"],
+        space_name=space_name
+    )
+    actions_logger.log_post(space_id)
+
+    if Settings.get().config["dareg"]["enabled"] and space_id:
+        dareg.register_dataset(space_id, dataset_name, base_path)
+
+    time.sleep(1 * Settings.get().config["sleepFactor"])
+
+    # Create support token
+    actions_logger.log_pre("temporary_token", "")
+    support_token = tokens.createTemporarySupportToken(space_id)
+    actions_logger.log_post(support_token)
+
+    actions_logger.log_pre("storage", dataset_name)
+    # Create storage for the space
+    storage_id = storages.createAndGetStorage(
+        name=space_name,
+        mountpoint=os.path.join(base_path, directory.name)
+    )
+    actions_logger.log_post(storage_id)
+
+    if Settings.get().config["dareg"]["enabled"] and storage_id:
+        dareg.log(space_id, "info", "created storage %s" % storage_id)
+    time.sleep(3 * Settings.get().config["sleepFactor"])
+
+    if not space_id or not support_token:
+        Logger.log(1, "Space for %s not created." % directory.name)
+        return False
+
+    actions_logger.log_pre("space_support", "")
+    # set up space support on the provider
+    result_support = spaces.supportSpace(
+        token=support_token,
+        size=Settings.get().config["implicitSpaceSize"],
+        storage_id=storage_id,
+        space_id=space_id
+    )
+    time.sleep(2 * Settings.get().config["sleepFactor"])
+    actions_logger.log_post(result_support)
+
+    if Settings.get().config["dareg"]["enabled"] and result_support:
+        dareg.log(space_id, "info", "supported")
+
+    # TODO:
+    if Settings.get().DATA_REPLICATION_ENABLED:
+        Logger.log(
+            3, "Setting up replication of space %s" % space_id
+        )
+        _add_support_from_all(support_token, space_id)
+        _add_qos_requirement(space_id, Settings.get().DATA_REPLICATION_REPLICAS)
+
+    # TODO: resolve in rollback
+    """
+    # HACK
+    if not result_support:
+        # delete space
+        spaces.removeSpace(space_id)
+        time.sleep(2 * Settings.get().config["sleepFactor"])
+
+        # delete storage
+        storages.removeStorage(storage_id)
+        Logger.log(
+            1, "Space for %s not created (unsucessfull support)." % directory.name
+        )
+        if Settings.get().config["dareg"]["enabled"] and result_support:
+            dareg.log(space_id, "error", "removed")
+        return
+    """
+
+    actions_logger.log_pre("group", space_name)
+    # Create user group for space
+    gid = groups.createChildGroup(
+        parent_group_id=Settings.get().config["userGroupId"],
+        group_name=space_name
+    )
+    actions_logger.log_post(gid)
+
+    if Settings.get().config["dareg"]["enabled"] and gid:
+        dareg.log(space_id, "info", "created group %s" % gid)
+    time.sleep(1 * Settings.get().config["sleepFactor"])
+
+    actions_logger.log_pre("token", space_name)
+    # Create invite token for the user group
+    token = tokens.createInviteTokenToGroup(
+        group_id=gid,
+        token_name=space_name
+    )
+    actions_logger.log_post(token)
+
+    if Settings.get().config["dareg"]["enabled"] and token:
+        dareg.log(space_id, "info", "created invite token")
+    time.sleep(1 * Settings.get().config["sleepFactor"])
+
+    # add the space to the user group
+    privileges = [
+        "space_view",
+        "space_view_privileges",
+        "space_read_data",
+        "space_add_support",
+    ]
+
+    actions_logger.log_pre("group_to_space", "")
+    response = spaces.addGroupToSpace(
+        space_id=space_id,
+        gid=gid,
+        privileges=privileges
+    )
+    actions_logger.log_post(response)
+
+    if Settings.get().config["dareg"]["enabled"] and response: # response is valid object, removed todo
+        dareg.log(space_id, "info", "group added to space")
+    time.sleep(1 * Settings.get().config["sleepFactor"])
+
+    # not logging, only filesystem operation
+    # write onedata parameters (space_id, invite_token) to file
+    yaml_onedata_dict = dict()
+    yaml_onedata_dict[Settings.get().config["metadataFileTags"]["space"]] = space_id
+    yaml_onedata_dict[Settings.get().config["metadataFileTags"]["inviteToken"]] = token["token"]
+    filesystem.setValuesToYaml(yml_file, yml_content, yaml_onedata_dict)
+    time.sleep(3 * Settings.get().config["sleepFactor"])
+
+
+    actions_logger.log_pre("auto_storage_import", "")
+    # first import of files to Onedata space
+    response = spaces.startAutoStorageImport(space_id)
+    time.sleep(3 * Settings.get().config["sleepFactor"])
+    actions_logger.log_post(response.ok)
+
+    # TODO: tu som skoncil
+
+    # set up permissions
+    file_id = spaces.getSpace(space_id)["fileId"]
+    files.setFileAttributeRecursive(
+        file_id, Settings.get().config["initialPOSIXlikePermissions"]
+    )
+
+    # create public share
+    share = shares.createAndGetShare(dataset_name, file_id)
+    if not share:
+        Logger.log(1, "Share for the space %s not created." % dataset_name)
+        return
+
+    if Settings.get().config["dareg"]["enabled"]:
+        dareg.update_dataset(space_id, token["token"], share["publicUrl"])
+
+    # add Share description
+    with open("share_description_base.md", "r") as file:
+        to_substitute = {
+            "dataset_name": dataset_name,
+            "institution_name": Settings.get().config["institutionName"],
+            "share_file_id": share["rootFileId"],
+        }
+        src = Template(file.read())
+        result = src.substitute(to_substitute)
+        shares.updateShare(share["shareId"], description=result)
+
+    # write onedata parameter (publicURL) to file
+    filesystem.setValueToYaml(
+        yml_file,
+        yml_content,
+        Settings.get().config["metadataFileTags"]["publicURL"],
+        share["publicUrl"],
+    )
+
+    # set metadata for the space
+    if Settings.get().config["importMetadata"]:
+        metadata.setSpaceMetadataFromYaml(space_id)
+
+    path = base_path + os.sep + directory.name
+    Logger.log(3, "Processing of %s done." % path)
+    if Settings.get().config["dareg"]["enabled"]:
+        dareg.log(space_id, "info", "processing done")
+    time.sleep(3 * Settings.get().config["sleepFactor"])
+
+
 
 
 # TODO - WIP
