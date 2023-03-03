de63205988668440a068210f9f4102b38993d6ac
Jozef Sabo <536644@mail.muni.cz>
2023-03-03 11:59:11 +0100

    Added more tries when doing rollback

--- app/rollback_actions.py
+++ app/rollback_actions.py
@@ -6,6 +6,8 @@ import time
 from utils import Settings
 from utils import Logger
 
+NUMBER_OF_TRIES_IF_NOT_FOUND = 3
+
 
 def action_space(space_name: str, space_id: str) -> bool:
     """
@@ -22,19 +24,30 @@ def action_space(space_name: str, space_id: str) -> bool:
         return False
 
     space = None
+    search_successful = False
 
-    Logger.log(3, f"rollback - getting space from id {space_id}")
-    if space_id:
-        space = spaces.get_space_from_onezone(space_id)  # space got, no need to change space id
+    for try_number in range(1, NUMBER_OF_TRIES_IF_NOT_FOUND + 1):
+        Logger.log(3, f"rollback - getting space from id {space_id}, try {try_number}/{NUMBER_OF_TRIES_IF_NOT_FOUND}")
+        if space_id:
+            space = spaces.get_space_from_onezone(space_id)  # space got, no need to change space id
 
-    if not space:
+        if space:
+            search_successful = True
+            break
+
         Logger.log(3, f"rollback - id is not correct, getting from name {space_name}")
         space_id = spaces.get_space_id_by_name(space_name)
 
-        # it is ok, because we do not have to delete space
-        if not space_id:
-            Logger.log(3, "rollback - space does not exist on server, everything is OK")
-            return True
+        if space_id:
+            search_successful = True
+            break
+
+        time.sleep(3 * Settings.get().config["sleepFactor"])
+
+    # it is ok, because we do not have to delete space
+    if not search_successful:
+        Logger.log(3, "rollback - space does not exist on server, everything is OK")
+        return True
 
     Logger.log(3, f"rollback - removing space with id {space_id}")
     response = spaces.removeSpace(space_id)
@@ -60,19 +73,30 @@ def action_storage(storage_name: str, storage_id: str) -> bool:
         return False
 
     storage = None
+    search_successful = False
 
-    Logger.log(3, f"rollback - getting storage from id {storage_id}")
-    if storage_id:
-        storage = storages.getStorageDetails(storage_id)  # storage got, no need to change storage id
+    for try_number in range(1, NUMBER_OF_TRIES_IF_NOT_FOUND + 1):
+        Logger.log(3, f"rollback - getting storage from id {storage_id}, try {try_number}/{NUMBER_OF_TRIES_IF_NOT_FOUND}")
+        if storage_id:
+            storage = storages.getStorageDetails(storage_id)  # storage got, no need to change storage id
 
-    if not storage:
+        if storage:
+            search_successful = True
+            break
+
         Logger.log(3, f"rollback - id is not correct, getting from name {storage_name}")
         storage_id = storages.get_storage_id_by_name(storage_name)
 
-        # it is ok, because we do not have to delete storage
-        if not storage_id:
-            Logger.log(3, "rollback - storage does not exist on server, everything is OK")
-            return True
+        if storage_id:
+            search_successful = True
+            break
+
+        time.sleep(3 * Settings.get().config["sleepFactor"])
+
+    # it is ok, because we do not have to delete storage
+    if not search_successful:
+        Logger.log(3, "rollback - storage does not exist on server, everything is OK")
+        return True
 
     Logger.log(3, f"rollback - removing storage with id {storage_id}")
     response = storages.removeStorage(storage_id)
@@ -97,19 +121,30 @@ def action_group(group_name: str, group_id: str):
         return False
 
     group = None
+    search_successful = False
 
-    Logger.log(3, f"rollback - getting group from id {group_id}")
-    if group_id:
-        group = groups.getGroupDetails(group_id)  # group got, no need to change group id
+    for try_number in range(1, NUMBER_OF_TRIES_IF_NOT_FOUND + 1):
+        Logger.log(3, f"rollback - getting group from id {group_id}, try {try_number}/{NUMBER_OF_TRIES_IF_NOT_FOUND}")
+        if group_id:
+            group = groups.getGroupDetails(group_id)  # group got, no need to change group id
 
-    if not group:
+        if group:
+            search_successful = True
+            break
+
         Logger.log(3, f"rollback - id is not correct, getting from name {group_name}")
         group_id = groups.get_group_id_by_name(group_name)
 
-        # it is ok, because we do not have to delete group
-        if not group_id:
-            Logger.log(3, "rollback - group does not exist on server, everything is OK")
-            return True
+        if group_id:
+            search_successful = True
+            break
+
+        time.sleep(3 * Settings.get().config["sleepFactor"])
+
+    # it is ok, because we do not have to delete group
+    if not search_successful:
+        Logger.log(3, "rollback - group does not exist on server, everything is OK")
+        return True
 
     Logger.log(3, f"rollback - removing group with id {group_id}")
     response = groups.removeGroup(group_id)
@@ -133,22 +168,31 @@ def action_token(token_name: str, token_id: str) -> bool:
         return False
 
     token = None
+    search_successful = False
 
-    Logger.log(3, f"rollback - getting token from id {token_id}")
-    if token_id:
-        token = tokens.getNamedToken(token_id)  # token got, no need to change token id
+    for try_number in range(1, NUMBER_OF_TRIES_IF_NOT_FOUND + 1):
+        Logger.log(3, f"rollback - getting token from id {token_id}, try {try_number}/{NUMBER_OF_TRIES_IF_NOT_FOUND}")
+        if token_id:
+            token = tokens.getNamedToken(token_id)  # token got, no need to change token id
 
-    if not token:
+        if token:
+            search_successful = True
+            break
+
         Logger.log(3, f"rollback - id is not correct, getting from name {token_name}")
         response = tokens.get_users_named_token_by_name(token_name)
 
-        # it is ok, because we do not have to delete token
-        if response.status_code == 404:
-            Logger.log(3, "rollback - token does not exist on server, everything is OK")
-            return True
+        if response.ok:
+            token_id = response.json()["id"]
+            search_successful = True
+            break
 
-        # TODO response moze dat aj iny status code, crash chance
-        token_id = response.json()["id"]
+        time.sleep(3 * Settings.get().config["sleepFactor"])
+
+    # it is ok, because we do not have to delete token
+    if not search_successful:
+        Logger.log(3, "rollback - token does not exist on server, everything is OK")
+        return True
 
     Logger.log(3, f"rollback - removing token with id {token_id}")
     response = tokens.deleteNamedToken(token_id)
