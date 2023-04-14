0880c2b090efdf690be005422dba5731fcaf20a2
Jozef Sabo <536644@mail.muni.cz>
2023-04-14 12:11:56 +0200

    refactored get_transfer_status()

--- app/transfers.py
+++ app/transfers.py
@@ -4,16 +4,35 @@ from utils import Logger
 import request
 
 
-def getTransferStatus(transfer_id):
+def get_transfer_status(transfer_id: str, status_type_if_not_found: str = "transferStatus") -> dict:
+    """
+    Returns transfer info for given transfer id.
+    If transfer was not found, it is considered as completed and removed further by Onedata.
+    If that happens, status which is checked (provided in variable status_type_if_not_found) is set to value "not_found"
+    Any other error returns empty dict
+    >>> # possible usage - after transfer creation, having transfer id, transfer type was set to replication
+    >>> status = get_transfer_status(transfer_id, "replicationStatus")
+    >>> if not status: # internal error, not sufficient rights
+    >>>     # do stuff
+    >>> if status["replicationStatus"] == "not_found":
+    >>>     # you know that it is not present, do stuff
+    >>> status = status["replicationStatus"] # now can be anything from documentation
+    """
     Logger.log(4, "getTransferStatus(%s):" % transfer_id)
     # https://onedata.org/#/home/api/stable/oneprovider
     url = "oneprovider/transfers/" + transfer_id
-    response = request.get(url)
-    return response.json()
+    response = request.get(url, ok_statuses=(200, 404))
+
+    if response.ok:
+        return response.json()
+    elif response.status_code == 404:
+        return {status_type_if_not_found: "not_found"}
+    else:
+        return {}
 
 
 # WIP
-def createTransfer(type, replicating_provider_id, file_id, dataSourceType):
+def createTransfer(type, replicating_provider_id, file_id, dataSourceType) -> dict:
     Logger.log(
         4,
         "createTransfer(%s, %s, %s, %s):"
@@ -33,7 +52,7 @@ def createTransfer(type, replicating_provider_id, file_id, dataSourceType):
     if response.ok:
         return response.json()
     else:
-        return response
+        return {}
 
 
 # WIP
